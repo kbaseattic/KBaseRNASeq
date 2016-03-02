607d7b865f917189ce1884b9d7e767056ca893eb
Roman Sutormin <rsutormin@lbl.gov>
2016-03-02 13:34:57 -0800

    Changes related to compatibility with kb-sdk test mode (not done)

--- test/script_test/run_tests.sh
+++ test/script_test/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export KB_RUNTIME=/kb/runtime
 export PYTHONPATH="/mnt/kb/dev_container/modules/KBaseRNASeq/lib"
+export KB_DEPLOYMENT_CONFIG=/kb/module/deploy.cfg
 python /mnt/kb/dev_container/modules/KBaseRNASeq/test/script_test/basic_test.py $1 $2 $3
